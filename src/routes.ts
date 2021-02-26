0aaaddbefeab6d92250b09a7268b8333f33e9c43
Tarcisio Caetano <tarcisiocaetano2009@gmail.com>
2021-02-26 16:47:17 -0300

    envio e alteração de notas do usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AnswerController } from "./controllers/AnswerController";
 import { SendMailController } from "./controllers/SendMailController";
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
@@ -8,6 +9,7 @@ const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 // Rota para gerenciamento de usuários
 router.post("/users", userController.create);
@@ -19,4 +21,6 @@ router.get("/surveys", surveysController.show);
 // Rota para gerenciamento de emails/surveys_users
 router.post("/sendMail", sendMailController.execute);
 
+// Rota para alteração de respostas
+router.get("/answers/:value", answerController.execute);
 export { router };
