1da5fd074573858ff14d5ec80743803630a2504e
Tarcisio Caetano <tarcisiocaetano2009@gmail.com>
2021-02-26 16:09:59 -0300

    correção busca no banco e template

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -35,20 +35,21 @@ class SendMailController {
 
     const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
 
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: { user_id: user.id, value: null },
+      relations: ["user", "survey"],
+    });
+
     const variables = {
       name: user.name,
       title: survey.title,
       description: survey.description,
-      user_id: user.id,
+      id: "",
       link: process.env.URL_MAIL,
     };
 
-    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [{ user_id: user.id }, { value: null }],
-      relations: ["user", "survey"],
-    });
-
     if (surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id;
       await SendMailService.execute(email, survey.title, variables, npsPath);
       return res.status(201).json(surveyUserAlreadyExists);
     }
@@ -61,6 +62,8 @@ class SendMailController {
 
     await surveysUsersRepository.save(surveyUser);
 
+    // Enviar Email para usuário
+    variables.id = surveyUser.id;
     await SendMailService.execute(email, survey.title, variables, npsPath);
 
     return res.status(201).json(surveyUser);
